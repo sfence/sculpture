306924d7fe58e41708cb0da9c0a1c700b8a391f9
SFENCE <sfence.software@gmail.com>
2021-11-05 08:17:07 +0100

    Fix chisel recipe colision with darkage/hades_darkage mod.

--- crafting.lua
+++ crafting.lua
@@ -4,11 +4,17 @@ local S = sculpture.translator
 local items = {
   steel_ingot = "default:steel_ingot",
   stick = "default:stick",
+  iron_stick = "default:steel_ingot", 
 }
+
+if minetest.get_modpath("darkage") then
+  items.iron_stick = "darkage:iron_stick"
+end
 
 if minetest.get_modpath("hades_core") then
   items.steel_ingot = "hades_core:steel_ingot"
   items.stick = "hades_core:stick"
+  items.iron_stick = "hades_core:steel_rod"
 end
   
 minetest.register_craft({
@@ -32,8 +38,8 @@ if true then
   minetest.register_craft({
       output = "sculpture:chisel_stonemason",
       recipe = {
-        {items.steel_ingot},
-        {items.steel_ingot},
+        {items.iron_stick},
+        {items.iron_stick},
       },
     })
 end
